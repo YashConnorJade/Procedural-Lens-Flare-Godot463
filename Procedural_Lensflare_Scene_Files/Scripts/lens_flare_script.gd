23f2f4e0289a09120e2ab5aa350b62e8fc10b9c2
YashConnorJade <yashpandey2512@gmail.com>
2026-06-18 21:26:04 +0530

    Added support for user to change the colors of the halo rings from shader material's inspector window.

---
+++ Procedural_Lensflare_Scene_Files/Scripts/lens_flare_script.gd
@@ -0,0 +1,5 @@
+# Put the Procedural Lens Flare scene as a child of the light source.
+# Lensflare Halos will be made in relative of the viewing angle.
+
+extends Node3D
+# ******************* put your code here *******************lol
